e26ae96c7843460fd913f079b67b9dc53d39750a
arjun ajith <arjun@MSSHI7.asiapacific.hpqcorp.net>
2019-05-02 15:18:37 +0530

    completed router wrapping

--- main.go
+++ main.go
@@ -2,35 +2,66 @@ package main
 
 import (
 	"github.com/arjunajithtp/router-wrapper/router"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	r := router.Config{
 		Port: "1234",
-		Routes: []router.Route{
-			router.Route{
-				Method: http.MethodGet,
-				Path: "/id",
-				Handler: TestHandler1,
+		Groups: []router.Group{
+			router.Group{
+				Prefix: "/redfish/v1",
+				Middlewares: []echo.MiddlewareFunc{
+					middleware.Logger(),
+					middleware.Recover(),
+				},
+				Routes: []router.Route{
+					router.Route{
+						Method:  http.MethodGet,
+						Path:    "/get",
+						Handler: GetHandler,
+					},
+					router.Route{
+						Method:  http.MethodPost,
+						Path:    "/post/:id",
+						Handler: PostHandler,
+					},
+				},
 			},
-			router.Route{
-				Method: http.MethodPost,
-				Path: "/name",
-				Handler: TestHandler2,
+			router.Group{
+				Prefix: "/aggregator",
+				Middlewares: []echo.MiddlewareFunc{
+					middleware.Logger(),
+					middleware.Recover(),
+				},
+				Routes: []router.Route{
+					router.Route{
+						Method:  http.MethodGet,
+						Path:    "/get",
+						Handler: GetHandler,
+					},
+					router.Route{
+						Method:  http.MethodPost,
+						Path:    "/post/:id",
+						Handler: PostHandler,
+					},
+				},
 			},
 		},
 	}
-
 	r.NewRouter()
 }
 
-func TestHandler1(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Hello id")
+// GetHandler is for get method
+func GetHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Hello Get")
 }
 
-func TestHandler2(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Hello name")
+// PostHandler is for post method
+func PostHandler(c echo.Context) error {
+	data := "Hello Post: " + c.Param("id")
+	return c.JSON(http.StatusOK, data)
 }
--- router/wrapper.go
+++ router/wrapper.go
@@ -1,39 +1,48 @@
 package router
 
 import (
-	"net/http"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 // Config is the configration to create new router
 type Config struct {
 	Port   string
-	Routes []Route
+	Groups []Group
+}
+
+// Group is for creating groups in router
+type Group struct {
+	Prefix      string
+	Middlewares []echo.MiddlewareFunc
+	Routes      []Route
 }
 
 // Route is the router config
 type Route struct {
 	Method  string
 	Path    string
-	Handler func(echo.Context) error
+	Handler echo.HandlerFunc
 }
 
 // NewRouter creates new instance of router
 func (c *Config) NewRouter() {
 	e := echo.New()
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	for _, group := range c.Groups {
+		g := e.Group(group.Prefix)
+		// Middleware
+		g.Use(group.Middlewares...)
 
-	for _, route := range c.Routes {
-		switch route.Method {
-		case http.MethodGet:
-			e.GET(route.Path, route.Handler)
-		case http.MethodPost:
-			e.POST(route.Path, route.Handler)
+		for _, route := range group.Routes {
+			switch route.Method {
+			case http.MethodGet:
+				g.GET(route.Path, route.Handler)
+			case http.MethodPost:
+				g.POST(route.Path, route.Handler)
+			}
 		}
 	}
+
 	e.Logger.Fatal(e.Start(":" + c.Port))
 }
